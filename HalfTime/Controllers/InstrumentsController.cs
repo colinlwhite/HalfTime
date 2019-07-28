e6cb02fcc9e6ebafc1310ebedccb17e6b6dfd71c
Colin White <clwhite814@gmail.com>
2019-07-27 19:40:21 -0500

    getting all of the user's instruments back

---
+++ HalfTime/Controllers/InstrumentsController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HalfTime.Data;
+
+namespace HalfTime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class InstrumentsController : ControllerBase
+    {
+
+        readonly instrumentsRepository _instrumentsRepository;
+
+        public InstrumentsController()
+        {
+            _instrumentsRepository = new instrumentsRepository();
+        }
+
+        // GET: api/Instruments/5
+        [HttpGet("{id}")]
+        public ActionResult GetInstrumentsByUserId(int id)
+        {
+            var userInstruments = _instrumentsRepository.getUserInstruments(id);
+
+            return Ok(userInstruments);
+        }
+
+        // POST: api/Instruments
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT: api/Instruments/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ HalfTime/Data/instrumentsRepository.cs
@@ -0,0 +1,27 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HalfTime.Models;
+using System.Data.SqlClient;
+
+namespace HalfTime.Data
+{
+    public class instrumentsRepository
+    {
+
+        const string ConnectionString = "Server=localhost;Database=HalfTime;Trusted_Connection=True;";
+
+        public IEnumerable <Instrument> getUserInstruments(int id)
+        {
+            var sql = "select Instrument.Id, Instrument.Name, Instrument.Condition, Instrument.Category, Instrument.StudentId, Instrument.Brand, Instrument.ModelNumber from Instrument join UserInstrumentJoin on Instrument.Id = UserInstrumentJoin.InstrumentId join [User] u on UserInstrumentJoin.UserId = u.Id where u.Id = @id";
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var instruments = db.Query<Instrument>(sql, new { id }).ToList();
+                return instruments;
+            }
+            
+        }
+    }
+}
