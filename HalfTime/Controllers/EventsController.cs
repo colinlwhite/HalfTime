08e4b83894a8632c6cb270b955dadc64f8e32f44
Colin White <clwhite814@gmail.com>
2019-07-29 18:21:57 -0500

    full crud working on events

--- HalfTime/Controllers/EventsController.cs
+++ HalfTime/Controllers/EventsController.cs
@@ -13,36 +13,50 @@ namespace HalfTime.Controllers
     [ApiController]
     public class EventsController : ControllerBase
     {
-        // GET: api/Events
-        [HttpGet]
-        public IEnumerable<string> Get()
+        readonly eventsRepository _eventsRepository;
+
+        public EventsController()
         {
-            return new string[] { "value1", "value2" };
+            _eventsRepository = new eventsRepository();
         }
 
         // GET: api/Events/5
-        //[HttpGet("{id}", Name = "Get")]
-        //public string Get(int id)
-        //{
-        //    return "value";
-        //}
+        [HttpGet("{id}")]
+        public ActionResult GetEventsByUserId(int id)
+        {
+            var userEvents = _eventsRepository.getUserEvents(id);
+
+            return Ok(userEvents);
+        }
 
         // POST: api/Events
         [HttpPost]
-        public void Post([FromBody] string value)
+        public ActionResult AddEvent(Event createEvent)
         {
+            var newEvent = _eventsRepository.AddEvent(createEvent);
+
+            return Created($"api/events/{newEvent.Id}", newEvent);
         }
 
         // PUT: api/Events/5
         [HttpPut("{id}")]
-        public void Put(int id, [FromBody] string value)
+        public ActionResult updateEvent(int id, Event eventToUpdate)
         {
+            if (id != eventToUpdate.Id)
+            {
+                return BadRequest(new { Error = "There was an error" });
+            }
+            var updatedEvent = _eventsRepository.updateEvent(eventToUpdate);
+            return Ok(updatedEvent);
         }
 
         // DELETE: api/ApiWithActions/5
         [HttpDelete("{id}")]
-        public void Delete(int id)
+        public ActionResult DeleteEvent(int id)
         {
+            _eventsRepository.DeleteEvent(id);
+
+            return Ok("The event was deleted");
         }
     }
 }
--- HalfTime/Data/eventsRepository.cs
+++ HalfTime/Data/eventsRepository.cs
@@ -10,5 +10,88 @@ namespace HalfTime.Data
 {
     public class eventsRepository
     {
+
+        const string ConnectionString = "Server=localhost;Database=HalfTimeDB;Trusted_Connection=True;";
+
+        public IEnumerable<Event> getUserEvents(int id)
+        {
+            var sql = "select Event.Name, Event.Description, Event.Type, Event.Date, Event.Street, Event.City, Event.State, Event.ZipCode from Event join UserEventJoin on Event.Id = UserEventJoin.EventId join [User] u on UserEventJoin.UserId = u.Id where u.Id = @id";
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var events = db.Query<Event>(sql, new { id }).ToList();
+                return events;
+            }
+
+        }
+
+        public Event AddEvent(Event newEventObj)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var newEvent = db.QueryFirstOrDefault<Event>(@"
+                    Insert into event(name, description, type, date, street, city, state, zipcode)
+                    Output inserted.*
+                    Values(@name, @description, @type, @date, @street, @city, @state, @zipcode)",
+                    new
+                    {
+                        newEventObj.Name,
+                        newEventObj.Description,
+                        newEventObj.Type,
+                        newEventObj.Date,
+                        newEventObj.Street,
+                        newEventObj.City,
+                        newEventObj.State,
+                        newEventObj.ZipCode
+                    });
+
+                if (newEvent != null)
+                {
+                    return newEvent;
+                }
+            }
+
+            throw new Exception("Unfortunatley, a new event was not created");
+        }
+
+        public Event updateEvent(Event eventToUpdate)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var updateQuery = @"Update Event
+                                    Set Name = @name,
+                                    Description = @description,
+                                    Type = @type,
+                                    Date = @date,
+                                    Street = @street,
+                                    City = @city,
+                                    State = @state
+                                    Where Id = @id";
+
+                var rowsAffected = db.Execute(updateQuery, eventToUpdate);
+
+                if (rowsAffected == 1)
+                {
+                    return eventToUpdate;
+                }
+                throw new Exception("We could not update the event");
+            }
+        }
+
+        public void DeleteEvent(int id)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var parameter = new { Id = id };
+
+                var deleteQuery = "Delete from Event where Id = @id";
+
+                var rowsAffected = db.Execute(deleteQuery, parameter);
+
+                if (rowsAffected != 1)
+                {
+                    throw new Exception("We couldn't delete the event at this time");
+                }
+            }
+        }
     }
 }
