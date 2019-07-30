5ec3fbde0eace8c3a2060f46845096020f1bb2b1
Colin White <clwhite814@gmail.com>
2019-07-30 14:14:25 -0500

    full CRUD on volunteers

---
+++ HalfTime/Controllers/VolunteersController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HalfTime.Data;
+using HalfTime.Models;
+
+namespace HalfTime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class VolunteersController : ControllerBase
+    {
+        readonly volunteersRepository _volunteersRepository;
+
+        public VolunteersController()
+        {
+            _volunteersRepository = new volunteersRepository();
+        }
+
+        // GET: api/Volunteers/5
+        [HttpGet("{id}")]
+        public ActionResult GetVolunteersByUserId(int id)
+        {
+            var userVolunteers = _volunteersRepository.getUserVolunteers(id);
+
+            return Ok(userVolunteers);
+        }
+
+        // POST: api/Volunteers
+        [HttpPost]
+        public ActionResult AddVolunteer(Volunteer createVolunteer)
+        {
+            var newVolunteer = _volunteersRepository.AddVolunteer(createVolunteer);
+
+            return Created($"api/volunteers/{newVolunteer.Id}", newVolunteer);
+        }
+
+        // PUT: api/Volunteers/5
+        [HttpPut("{id}")]
+        public ActionResult updateVolunteer(int id, Volunteer volunteerToUpdate)
+        {
+            if (id != volunteerToUpdate.Id)
+            {
+                return BadRequest(new { Error = "There was an error" });
+            }
+            var volunteer = _volunteersRepository.updateVolunteer(volunteerToUpdate);
+            return Ok(volunteer);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult DeleteVolunteer(int id)
+        {
+            _volunteersRepository.DeleteVolunteer(id);
+
+            return Ok("The volunteer was deleted");
+        }
+    }
+}
---
+++ HalfTime/Data/volunteersRepository.cs
@@ -0,0 +1,96 @@
+﻿using Dapper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using HalfTime.Models;
+using System.Data.SqlClient;
+
+namespace HalfTime.Data
+{
+    public class volunteersRepository
+    {
+
+        const string ConnectionString = "Server=localhost;Database=HalfTimeDB;Trusted_Connection=True;";
+
+        public IEnumerable<Volunteer> getUserVolunteers(int id)
+        {
+            var sql = "select Volunteer.Id, Volunteer.FirstName, Volunteer.LastName, Volunteer.Street, Volunteer.City, Volunteer.State, Volunteer.ZipCode, Volunteer.PhoneNumber from Volunteer join UserVolunteerJoin on Volunteer.Id = UserVolunteerJoin.VolunteerId join [User] u on UserVolunteerJoin.UserId = u.Id where u.Id = @id";
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var volunteers = db.Query<Volunteer>(sql, new { id }).ToList();
+                return volunteers;
+            }
+
+        }
+
+        public Volunteer AddVolunteer(Volunteer newVolunteerObj)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var newVolunteer = db.QueryFirstOrDefault<Volunteer>(@"
+                    Insert into volunteer(firstname, lastname, street, city, state, zipcode, phonenumber)
+                    Output inserted.*
+                    Values(@firstname, @lastname, @street, @city, @state, @zipcode, @phonenumber)",
+                    new
+                    {
+                        newVolunteerObj.FirstName,
+                        newVolunteerObj.LastName,
+                        newVolunteerObj.Street,
+                        newVolunteerObj.City,
+                        newVolunteerObj.State,
+                        newVolunteerObj.ZipCode,
+                        newVolunteerObj.PhoneNumber,
+                    });
+
+                if (newVolunteer != null)
+                {
+                    return newVolunteer;
+                }
+            }
+
+            throw new Exception("Unfortunatley, a new volunteer was not created");
+        }
+
+        public Volunteer updateVolunteer(Volunteer volunteerToUpdate)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var updateQuery = @"Update Volunteer
+                                    Set FirstName = @firstname,
+                                    LastName = @lastname,
+                                    Street = @street,
+                                    City = @city,
+                                    State = @state,
+                                    ZipCode = @zipcode,
+                                    PhoneNumber = @phonenumber
+                                    Where Id = @id";
+
+                var rowsAffected = db.Execute(updateQuery, volunteerToUpdate);
+
+                if (rowsAffected == 1)
+                {
+                    return volunteerToUpdate;
+                }
+                throw new Exception("We could not update the volunteer");
+            }
+        }
+
+        public void DeleteVolunteer(int id)
+        {
+            using (var db = new SqlConnection(ConnectionString))
+            {
+                var parameter = new { Id = id };
+
+                var deleteQuery = "Delete from Volunteer where Id = @id";
+
+                var rowsAffected = db.Execute(deleteQuery, parameter);
+
+                if (rowsAffected != 1)
+                {
+                    throw new Exception("We couldn't delete the volunteer at this time");
+                }
+            }
+        }
+    }
+}
