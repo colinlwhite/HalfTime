d1877498ccdf7d383ba08290bd9bc340206f5849
Colin White <clwhite814@gmail.com>
2019-07-29 19:34:41 -0500

    basic CRUD on one user is enabled

---
+++ HalfTime/Controllers/UsersController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using HalfTime.Data;
+using HalfTime.Models;
+
+namespace HalfTime.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        readonly usersRepository _usersRepository;
+
+        public UsersController()
+        {
+            _usersRepository = new usersRepository();
+        }
+
+        // GET: api/Users/5
+        [HttpGet("{id}")]
+        public ActionResult GetUsersByUserId(int id)
+        {
+            var userUsers = _usersRepository.getUser(id);
+
+            return Ok(userUsers);
+        }
+
+        // POST: api/Users
+        [HttpPost]
+        public ActionResult AddUser(User createUser)
+        {
+            var newUser = _usersRepository.AddUser(createUser);
+
+            return Created($"api/users/{newUser.Id}", newUser);
+        }
+
+        // PUT: api/Users/5
+        [HttpPut("{id}")]
+        public ActionResult updateUser(int id, User userToUpdate)
+        {
+            if (id != userToUpdate.Id)
+            {
+                return BadRequest(new { Error = "There was an error" });
+            }
+            var updatedUser = _usersRepository.updateUser(userToUpdate);
+            return Ok(updatedUser);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public ActionResult DeleteUser(int id)
+        {
+            _usersRepository.DeleteUser(id);
+
+            return Ok("The user was deleted");
+        }
+    }
+}
---
+++ HalfTime/Models/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HalfTime.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string FireBaseUid { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Street { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public int ZipCode { get; set; }
+        public string PhoneNumber { get; set; }
+        public bool isVolunteer { get; set; }
+    }
+}
